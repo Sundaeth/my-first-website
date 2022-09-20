2715434f989d21b49ff2477cc04f1bb15cb113bd
Sun <potsaweeth@gmail.com>
2022-09-20 14:34:47 +0700

    feat:Complete

---
+++ src/components/DisplayJohn.js
@@ -0,0 +1,19 @@
+function DisplayJohn() {
+	return <h1>Hello, I'm John</h1>
+}
+
+export function JohnProfile() {
+	return (
+		<div>
+			<ul>
+				<li>Full Stack Developer</li>
+				<li>React.JS Developer</li>
+				<li>Love Mookrata</li>
+        <li>Also Love you</li>  
+			</ul>
+		</div>
+	)
+}
+
+
+export default DisplayJohn;
